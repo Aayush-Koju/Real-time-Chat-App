cb5d92b35ab5edff8fdb7458133c773d5fd893b7
Aayush-Koju <iush.cozu12@gmail.com>
2024-08-07 11:07:38 +0545

    linked routes to components in navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,8 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./Home";
+import ChatRoom from "./ChatRoom";
+import UserProfile from "./UserProfile";
+import Settings from "./Settings";
 
 function NavBar() {
   return (
@@ -10,18 +13,21 @@ function NavBar() {
             <Link to="/">Home</Link>
           </li>
           <li>
-            <Link to="/chat-room">Chat Room</Link>
+            <Link to="/chatroom">Chat Room</Link>
           </li>
           <li>
-            <Link to="/">User Profile</Link>
+            <Link to="/userprofile">User Profile</Link>
           </li>
           <li>
-            <Link to="/">Settings</Link>
+            <Link to="/settings">Settings</Link>
           </li>
         </ul>
 
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/chatroom" element={<ChatRoom />} />
+          <Route path="/userprofile" element={<UserProfile />} />
+          <Route path="/settings" element={<Settings />} />
         </Routes>
       </Router>
     </>
