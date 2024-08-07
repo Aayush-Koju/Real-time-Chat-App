5984d0227f3ab6e5da1d593786eff90e1e220262
Aayush-Koju <iush.cozu12@gmail.com>
2024-08-07 13:29:53 +0545

    create home component ui

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,25 @@
+import { Link } from "react-router-dom";
 export default function Home() {
   return (
     <>
-      <h1>Home Page</h1>
-      <h2>Welcome to the Real-time Chat Application</h2>
+      <h1>Welcome to the Real-time Chat Application</h1>
+      <p>Your one step destination for real-time chatting</p>
+
+      <div className="overview">
+        <h2>Features</h2>
+        <ul>
+          <li>Create and join chat rooms</li>
+          <li>Send and receive messages</li>
+          <li>Manage your user profile</li>
+          <li>Customize your settings</li>
+        </ul>
+      </div>
+
+      <div>
+        <p>Since You have joined for the first time. Please register.</p>
+
+        <Link to="/userprofile">Register</Link>
+      </div>
     </>
   );
 }
